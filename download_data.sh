37ecc50becf886abe019685c766a4adb2bb4ddfa
Nurzhan <32760183+sarzhann@users.noreply.github.com>
2019-06-24 10:16:54 +0200

    Added downloading of weigths

--- download_data.sh
+++ download_data.sh
@@ -1,3 +1,5 @@
 # Download dataset
 wget "https://www.dropbox.com/s/6hdhn8jpgw1mjr6/data.zip?dl=0"
 unzip -q "data.zip?dl=0"
+# Download pretrained weights
+wget "https://www.dropbox.com/s/a1iby1ywrnau1rb/classifier.pt?dl=0"
