3b8914c4178b62689395ee8fa9e0a03d5153fb2a
Nurzhan <32760183+sarzhann@users.noreply.github.com>
2019-06-26 12:13:14 +0200

    Run to download dataset and weights

---
+++ download_data.sh
@@ -0,0 +1,6 @@
+# Download dataset
+wget "https://www.dropbox.com/s/h94xfj7447m48lk/data.zip?dl=0"
+unzip -q "data.zip?dl=0"
+# Download pretrained weights
+wget "https://www.dropbox.com/s/gy6fy4izgqxeydj/classifier.pt?dl=0"
+wget "https://www.dropbox.com/s/nbgnf7fp775kmch/multiclass.pt?dl=0"
